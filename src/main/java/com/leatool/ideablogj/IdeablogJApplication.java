f79866d7c422962f8fca01684424ec0d5273a9e9
liu_ <13293920099@163.com>
2019-05-25 19:36:29 +0800

    first commit

---
+++ src/main/java/com/leatool/ideablogj/IdeablogJApplication.java
@@ -0,0 +1,13 @@
+package com.leatool.ideablogj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeablogJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeablogJApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/leatool/ideablogj/controller/Login.java
@@ -0,0 +1,16 @@
+package com.leatool.ideablogj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class Login {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getLogin(){
+        return "home";
+    }
+
+}
